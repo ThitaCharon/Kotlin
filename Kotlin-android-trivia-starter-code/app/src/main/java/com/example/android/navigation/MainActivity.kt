3f8cff7850675077388ad7fca7e1223a57aefc28
Thita Charoentham <thita.charoentham35@qmail.cuny.edu>
2019-05-17 11:52:23 -0400

    Navigation graph

--- Kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,12 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDestination
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +32,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val rootView = window.decorView.rootView
+        val navController  = this.findNavController(R.id.myNavHostFragment)
+       NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
